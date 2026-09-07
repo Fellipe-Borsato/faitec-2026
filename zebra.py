29dfc9560c351516e165b84c599f239e6ead426b
SouDescolado <moacirsega@gmail.com>
2026-09-07 17:12:20 -0300

    Atualizando a base com informações atualizadas.

--- zebra.py
+++ zebra.py
@@ -2,9 +2,10 @@ import random
 import os
 
 class zebra:
-    def __init__(self,chave='12345'*5):
+    def __init__(self,chave='12345'*6):
         self.path = r'./Categorias/'
-        self.chave=chave
+        self.chave=str(chave)
+
     def buscaCategorias(self,mostraValores=True):
         categorias = []
         for file in os.listdir(self.path):
@@ -14,6 +15,7 @@ class zebra:
                 else:
                     categorias.append(file.split('.')[0])
         return categorias
+
     def buscaCategoria(self,categoria):
         categoria = str(categoria)
         for file in os.listdir(self.path):
@@ -21,7 +23,7 @@ class zebra:
                 nome = file[2:].replace('.txt','')
         return nome
 
-    def buscaValores(self,categoria,mostraValores=True):
+    def buscaValores(self,categoria,mostraValores=True,mostraVerbo=False):
         categoria = str(categoria)
         valores = []
         for file in os.listdir(self.path):
@@ -32,13 +34,15 @@ class zebra:
                     if mostraValores:
                         valores.append(linha.strip())
                     else:
-                        valores.append(str(conteudo.index(linha)+1))
+                        valores.append(str(conteudo.index(linha)))
+        if not mostraVerbo:
+            valores.pop(0)
         return valores
     
     def buscaValor(self,dados):
-        posicao = int(dados[1])-1
+        posicao = int(dados[1])
         for file in os.listdir(self.path):
-            if file[0] == dados[0]:
+            if int(file[0]) == int(dados[0]):
                 fileopen = open(self.path + file, encoding="utf-8")
                 conteudo = fileopen.readlines()
                 return conteudo[posicao].strip()
@@ -58,9 +62,7 @@ class zebra:
     
     def geraChave(self):
         chave = ''
-        categorias = []
-        for file in os.listdir(self.path):
-            categorias.append(file[0])
+        categorias = self.buscaCategorias(False)
         escolhas = []
         for _ in range(5):
             escolha = random.randrange(len(categorias))
@@ -70,14 +72,27 @@ class zebra:
             for k in range(5):
                 escolha = random.randrange(len(valores))
                 chave+= valores.pop(escolha)
-                
-
         return chave
+
+    def geraDica(self,tipo,dados1,dados2):
+
+        if tipo.upper() == 'POSICIONAL':
+            extraichave = (self.chave[dados2[0]],self.chave[5*dados2[0]+dados2[1]])
+            print('---')
+            print(extraichave)
+            print('---')
+            dica = f'Na posição {dados2[1]} fica quem {self.buscaValor((int(extraichave[0]),0))} {self.buscaValor((int(extraichave[0]),int(extraichave[1])+1))}'.upper().replace('QUEM A', 'A').replace('É,O','O').replace('QUEM O', 'O')
+        else:
+            dica = f'{self.buscaValor((extraichave[0],0)).replace('É,','')} {self.buscaValor((extraichave[0],extraichave[1]))}'
+        extraichave = (self.chave[dados2[0]-1],self.chave[5*dados2[0]+dados2[1]])
+        
+        return dica
         
 
+        
 
-chavealeatoria = zebra().geraChave()
-print(chavealeatoria)
-print(zebra(chavealeatoria).pegaResposta())
-'''123451234512345123451234512345
-123451111122222333334444455555'''
+#print(zebra().pegaResposta())
+#chavealeatoria = zebra().geraChave()
+#print(chavealeatoria)
+#print(zebra(chavealeatoria).pegaResposta())
+#print(zebra(chavealeatoria).geraDica('posicional',(0,3),(3,3)))
